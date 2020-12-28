198d839fa366deb514070d2ae82b6896b6c1e23f
申 志云 <296932795@qq.com>
2020-12-28 17:20:09 +0800

    打印

---
+++ CameraPhoto/PrintPhoto.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CameraPhoto
+{
+    /// <summary>
+    /// PrintPhoto.xaml 的交互逻辑
+    /// </summary>
+    public partial class PrintPhoto : Window
+    {
+        public PrintPhoto()
+        {
+            InitializeComponent();
+
+            // 在此点之下插入创建对象所需的代码。
+            ImageBrush b = new ImageBrush();
+            b.ImageSource = new BitmapImage(new Uri("pack://application:,,,/CameraPhoto;component/Resources/bacbkground.png"));
+            b.Stretch = Stretch.Fill;
+            this.Background = b;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Maximized;
+        }
+    }
+}
---
+++ CameraPhoto/SelectFilter.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CameraPhoto
+{
+    /// <summary>
+    /// SelectFilter.xaml 的交互逻辑
+    /// </summary>
+    public partial class SelectFilter : Window
+    {
+        public SelectFilter()
+        {
+            InitializeComponent();
+
+            // 在此点之下插入创建对象所需的代码。
+            ImageBrush b = new ImageBrush();
+            b.ImageSource = new BitmapImage(new Uri("pack://application:,,,/CameraPhoto;component/Resources/bacbkground.png"));
+            b.Stretch = Stretch.Fill;
+            this.Background = b;
+            ImageBrush a = new ImageBrush();
+            a.ImageSource = new BitmapImage(new Uri("pack://application:,,,/CameraPhoto;component/Resources/背景块-滤镜选中.png"));
+            a.Stretch = Stretch.Fill;
+            filter1.Background = a;
+
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Maximized;
+        }
+    }
+}
